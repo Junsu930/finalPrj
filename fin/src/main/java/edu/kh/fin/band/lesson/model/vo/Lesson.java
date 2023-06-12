238145033097fc58b3161bf3b22325a759cfcafa
kms <kimrlaks@gmail.com>
2023-06-13 08:54:36 +0900

    commit

---
+++ fin/src/main/java/edu/kh/fin/band/lesson/model/vo/Lesson.java
@@ -0,0 +1,51 @@
+/*package edu.kh.fin.band.lesson.model.vo;
+
+public class Lesson {
+	
+	private int LessonID;
+	private String LessonTitle;
+	private String userID;
+	private String LessonDate;
+	private String LessonContent;
+	private int LessonAvailable;
+	
+	public int getLessonID() {
+		return LessonID;
+	}
+	public void setLessonID(int lessonID) {
+		LessonID = lessonID;
+	}
+	public String getLessonTitle() {
+		return LessonTitle;
+	}
+	public void setLessonTitle(String lessonTitle) {
+		LessonTitle = lessonTitle;
+	}
+	public String getUserID() {
+		return userID;
+	}
+	public void setUserID(String userID) {
+		this.userID = userID;
+	}
+	public String getLessonDate() {
+		return LessonDate;
+	}
+	public void setLessonDate(String lessonDate) {
+		LessonDate = lessonDate;
+	}
+	public String getLessonContent() {
+		return LessonContent;
+	}
+	public void setLessonContent(String lessonContent) {
+		LessonContent = lessonContent;
+	}
+	public int getLessonAvailable() {
+		return LessonAvailable;
+	}
+	public void setLessonAvailable(int lessonAvailable) {
+		LessonAvailable = lessonAvailable;
+	}
+
+
+}
+*/
