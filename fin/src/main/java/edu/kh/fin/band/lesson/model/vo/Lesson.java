cbfecf2678dd8cfa71c36f94d67288031f645e87
kms <kimrlaks@gmail.com>
2023-06-14 09:01:35 +0900

    commit

--- fin/src/main/java/edu/kh/fin/band/lesson/model/vo/Lesson.java
+++ fin/src/main/java/edu/kh/fin/band/lesson/model/vo/Lesson.java
@@ -1,51 +1,26 @@
-/*package edu.kh.fin.band.lesson.model.vo;
+package edu.kh.fin.band.lesson.model.vo;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Lesson {
 	
-	private int LessonID;
-	private String LessonTitle;
-	private String userID;
-	private String LessonDate;
-	private String LessonContent;
-	private int LessonAvailable;
-	
-	public int getLessonID() {
-		return LessonID;
-	}
-	public void setLessonID(int lessonID) {
-		LessonID = lessonID;
-	}
-	public String getLessonTitle() {
-		return LessonTitle;
-	}
-	public void setLessonTitle(String lessonTitle) {
-		LessonTitle = lessonTitle;
-	}
-	public String getUserID() {
-		return userID;
-	}
-	public void setUserID(String userID) {
-		this.userID = userID;
-	}
-	public String getLessonDate() {
-		return LessonDate;
-	}
-	public void setLessonDate(String lessonDate) {
-		LessonDate = lessonDate;
-	}
-	public String getLessonContent() {
-		return LessonContent;
-	}
-	public void setLessonContent(String lessonContent) {
-		LessonContent = lessonContent;
-	}
-	public int getLessonAvailable() {
-		return LessonAvailable;
-	}
-	public void setLessonAvailable(int lessonAvailable) {
-		LessonAvailable = lessonAvailable;
-	}
+	private int LessonNo;
+	private int userNo;
+	private String introment;
+	private String joindate;
+	private String lessonImg;
+	private String genre;
+	private String ints;
+	private String Region; 
+	private String detailment;
+	private String socialSite;
 
 
 }
-*/
