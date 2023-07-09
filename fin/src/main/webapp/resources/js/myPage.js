e69967b35d9eac03381d375aae351d1dd841dfc1
jungwoo <suj000777@gmail.com>
2023-07-09 09:57:25 +0900

    final commit

--- fin/src/main/webapp/resources/js/myPage.js
+++ fin/src/main/webapp/resources/js/myPage.js
@@ -202,4 +202,218 @@ showBtn.addEventListener('click', function(){
     }
 
   });
+
+  const userNick =document.getElementById("userNick");
+  const userPw =document.getElementById("userPw");
+  const userIntro =document.getElementById("userIntro");
+  const instLength = document.querySelectorAll('input[name="inst"]:checked');
+  const genreLength = document.querySelectorAll('input[name="genre"]:checked');
+  const regionLength = document.querySelectorAll('input[name="region"]:checked');
+
+  
+/*
+  const checkOb = {
+    "userNick" : false,
+    "userPw" : false,
+    "userIntro" : false,
+    "instLength" : false,
+    "genreLength" : false,
+    "regionLength": false
+
+  }
+
+instLength.addEventListener("change", function() {
+
+  if(instLength.length == 0){
+
+    checkOb.instLength = false;
+   
+ 
+  } else {
+    checkOb.instLength = true;
+  }
+  
+
+});
+
+regionLength.addEventListener("change", function() {
+
+  if(regionLength.length == 0){
+
+    checkOb.regionLength = false;
+   
+ 
+  } else {
+    checkOb.regionLength = true;
+  }
+  
+
+});
+
+genreLength.addEventListener("change", function() {
+
+  if(genreLength.length == 0){
+
+    checkOb.genreLength = false;
+   
+ 
+  } else {
+    checkOb.genreLength = true;
+  }
+  
+
+});
+
+
+*/
+
+
+
+// 닉네임 중복 검사
+const nickMessage = document.getElementById("nickMessage");
+
+  userNick.addEventListener("input", function() {
+
+    if(userNick.value.length == 0){
+
+      nickMessage.innerText = "";
+
+      checkOb.userNcik = false;
+      return;
+    }
+
+    const regExp = /^[a-zA-Z0-9가-힣]{2,10}$/;
+
+    if(regExp.test(userNick.value)) {
+
+
+
+      $.ajax({
+        url : "nicknameDupCheck",  // 필수 작성 속성
+        data : { "userNickname" : userNick.value }, // 서버로 전달할 값(파라미터)
+        type : "GET", // 데이터 전달 방식(기본값 GET)
+
+        success : function(res){ // 비동기 통신 성공 시(에러 발생 X)
+
+            // 매개변수 res : Servlet에서 응답으로 출력된 데이터가 저장
+
+            if(res == 0){ // 닉네임 중복 X
+              nickMessage.innerText = "";
+              nickMessage.classList.add("confirm");
+              nickMessage.classList.remove("error");
+                checkOb.userNick = true; // 유효 O 기록
+
+            } else { // 닉네임 중복 O
+              nickMessage.innerText = "Duplicated name";
+              nickMessage.classList.add("error");
+              nickMessage.classList.remove("confirm");
+                checkOb.userNick = false; // 유효 O 기록
+            }
+        },
+
+        error : function(){ // 비동기 통신 중 에러가 발생한 경우
+            console.log("에러 발생");
+        }
+    });
+    }
+  });
+
+
+  userPw.addEventListener("input", function(){
+
+    if(userPw.value.length == 0){
+        pMessage.innerText = "";
+        pMessage.classList.remove("confirm", "error");
+
+        checkOb.userPw = false; // 유효 X 기록
+        return;
+    }
+
+    const regExp = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{4,20}$/;
+
+    if( regExp.test(userPw.value) ){ // 비밀번호 유효
+
+            pMessage.innerText = "";
+            pMessage.classList.add("confirm");
+            pMessage.classList.remove("error");
+
+            checkOb.userPw = true;
+        
+
+    }else{
+        pMessage.innerText = "Invalid PW";
+        pMessage.classList.add("error");
+        pMessage.classList.remove("confirm");
+
+        checkOb.userPw = false; // 유효 X 기록
+    }
+});
+
+userIntro.addEventListener("input", function(){
+
+      if(userIntro.value.length == 0){
+          checkOb.userIntro = false;
+      }
+      const regExp = /^[.]{20}$/;
+
+      if(regExp.test(userIntro.value)) {
+        checkOb.userIntro = true;
+      }
+
+
+});
+
+
+  
+/*
+
+  function UpdateValidate(){
+
+    if(instLength == 0){
+      alert("POSITION 항목이 비었습니다!");
+     
+      return false;
+  }else if(genreLength == 0){
+      alert("GENRE 항목이 비었습니다!");
+     
+      return false;
+  }else if( regionLength == 0){
+      alert("REGION 항목이 비었습니다!");
+      return false;
+  } else {
+
+    return true;
+  }
+}
+
+function UpdateValidate(){
+
+  for( let key  in checkOb ){
+
+    let str;
+
+    if( !checkOb[key] ){ 
+
+        switch(key){
+          case "userNick" :     str="닉네임이"; break;
+          case "userPw" :       str="비밀번호가"; break;
+          case "userIntro" :    str="자기 소개가"; break;
+          case "instLength":    str="내 서칭 정보가"; break;
+          case "genreLength":   str="내 서칭 정보가"; break;
+          case "regionLength":  str="내 서칭 정보가"; break;      
+        }
+        str += " 유효하지 않음.";
+
+            alert(str);
+
+            document.getElementById(key).focus();
+            
+            return false; // form태그 기본 이벤트 제거
+        }
+    
+      }
+    return true; // form태그 기본 이벤트 수행
+
+  }
+ */
   
